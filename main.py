444eff51a5892c87b52e2b588f32017408672525
Guilherme freitas <guijetix27@gmail.com>
2026-01-09 03:05:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -384,6 +384,7 @@ if not st.session_state.df_erros.empty:
 
 
 st.title("🔄 Simulador de Trocas de Turno")
+st.write(st.session_state.df_escalas)
 
 if "df_escalas" not in st.session_state:
     st.session_state.df_escalas = pd.DataFrame()
@@ -396,7 +397,7 @@ operador_selecionado = st.selectbox(
     "Operador",
     st.session_state.df_escalas["Operador"].tolist()
 )
-st.write(st.session_state.df_escalas)
+
 dia_selecionado = st.selectbox(
     "Dia",
     list(range(1, 32))
