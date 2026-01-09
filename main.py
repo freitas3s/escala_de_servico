a8d386f27b6fe619166654dca8d4ae1533c79a1d
Guilherme freitas <guijetix27@gmail.com>
2026-01-09 03:10:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Escala", layout="wide")
 
 
 def aplicar_troca(df, operador_a, operador_b, dia):
-    col = f"D{dia}"
+    col = f"{dia} Ter"
 
     turno_a = df.loc[df["Nome"] == operador_a, col].values[0]
     turno_b = df.loc[df["Nome"] == operador_b, col].values[0]
